45296ffd5a280d190295e597efab34abe68912d9
everr <everrwsr@outlook.com>
2022-03-10 12:24:40 +0800

    循环之前

---
+++ demo1.py
@@ -0,0 +1,12 @@
+# -*- coding = utf-8 -*-
+# @Time : 2022/3/10
+# @Author : everr
+# @File : demo1.py
+# @Software: PyCharm
+
+print("hello,world!")
+'''
+这是多行注释
+第二行
+
+'''
---
+++ demo2.py
@@ -0,0 +1,25 @@
+# -*- coding = utf-8 -*-
+# @Time : 2022/3/10 11:39
+# @Author : everr
+# @File : demo2.py
+# @Software: PyCharm
+'''
+age = 19
+print("我的名字是：%s,我的国籍是%s"%("校长","china"))
+print("我的年纪是：%d 岁"%age)
+print('aaa',"bbb","ccc")
+print("www","baidu","com",sep=".")
+print("hello",end="")
+print()
+'''
+'''
+password = input("请输入密码")
+print("您刚刚输入的密码是：",password)
+'''
+# a =input("输入：")
+# print(type(a))
+# print("输入了一个数字：%s"%a)
+a= int("123")
+b=a+100
+print(b)
+c= int(input(""))
